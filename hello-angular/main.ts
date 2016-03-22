2297e2ba97264dc40901012e5d35a7063b7375c0
UcheEke <uche.eke@gmail.com>
2016-03-22 22:17:51 +0000

    Hello Angular 2 Example added

---
+++ hello-angular/main.ts
@@ -0,0 +1,20 @@
+// Imports
+import {Component} from "angular2/core";
+import {bootstrap} from "angular2/platform/browser";
+
+// Component configuration object
+var config: any = {
+    selector: 'hello', 
+    templateUrl: 'tpls/hello.html'
+};
+
+// Decorate the class HelloAngular (i.e. the Component) with the @Component metatag
+@Component(config) class HelloAngular {
+    name : string;
+    constructor() {
+        this.name = 'Angular 2!!';
+    }
+}
+
+// Start your component, with zero dependencies injected (hence the '[]' empty array)!
+bootstrap(HelloAngular, []);
